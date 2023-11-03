4d461442ab0563ca6a68b290fbd764752e66d4e7
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-03 19:33:05 +0100

    tailwind config for vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        amazon: {
+          background: "#ededed"
+        }
+      }
+    },
   },
   plugins: [],
 }
