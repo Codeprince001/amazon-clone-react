a5233aa743083e0f50a902587872607d74495c7c
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-03 19:46:25 +0100

    fixed source file of image tag

--- src/Components/Carousel/Carousel.jsx
+++ src/Components/Carousel/Carousel.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import "./carousel.css";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';
+import banner1 from "../../assets/banner-1.jpg";
+import banner2 from "../../assets/banner-2.jpg";
+import banner3 from "../../assets/banner-3.jpg";
+import banner4 from "../../assets/banner-4.jpg";
+import banner5 from "../../assets/banner-5.jpg";
+
+
+
 
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -23,11 +31,11 @@ function Carousel() {
         }}
         modules={[Pagination, Navigation, Autoplay]}
       >
-        <SwiperSlide><div className='swiper-slide-content'><img src='../src/assets/banner-1.jpg' className='slide-img' /></div></SwiperSlide>
-        <SwiperSlide><div className='swiper-slide-content'><img src='../src/assets/banner-2.jpg' className='slide-img' /></div></SwiperSlide>
-        <SwiperSlide><div className='swiper-slide-content'><img src='../src/assets/banner-3.jpg' className='slide-img' /></div></SwiperSlide>
-        <SwiperSlide><div className='swiper-slide-content'><img src='../src/assets/banner-4.jpg' className='slide-img' /></div></SwiperSlide>
-        <SwiperSlide><div className='swiper-slide-content'><img src='../src/assets/banner-5.jpg' className='slide-img' /></div></SwiperSlide>
+        <SwiperSlide><div className='swiper-slide-content'><img src={banner1} className='slide-img' /></div></SwiperSlide>
+        <SwiperSlide><div className='swiper-slide-content'><img src={banner2} className='slide-img' /></div></SwiperSlide>
+        <SwiperSlide><div className='swiper-slide-content'><img src={banner3} className='slide-img' /></div></SwiperSlide>
+        <SwiperSlide><div className='swiper-slide-content'><img src={banner4} className='slide-img' /></div></SwiperSlide>
+        <SwiperSlide><div className='swiper-slide-content'><img src={banner5} className='slide-img' /></div></SwiperSlide>
       </Swiper>
     </section>
   );
