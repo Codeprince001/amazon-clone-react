b38e04fb4051e0fcac9354cde97464e958d2417d
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-08 01:37:01 +0100

    component for default export

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from './Header/Header';
 export { default as Search } from "./Search";
 export { default as Card } from "./Card/Card";
 export { default as Carousel } from "./Carousel/Carousel";
+export { default as CarouselCategory } from "./Carousel/CarouselCategory";
