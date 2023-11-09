ffd9a864c851f97cef52275fa6e734f3d44ee454
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-09 16:46:20 +0100

    Desktop switch component

--- src/Components/DesktopModeSwitcher.jsx
+++ src/Components/DesktopModeSwitcher.jsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const DesktopModeSwitcher = () => {
   const [desktopMode, setDesktopMode] = useState(false);
 
-  const toggleDesktopMode = () => {
-    setDesktopMode(!desktopMode);
+  useEffect(() => {
+    toggleDesktopMode();
+  }, []);
 
-    // Change the window.innerWidth to simulate desktop mode
+  const toggleDesktopMode = () => {
+    setDesktopMode(prevMode => !prevMode);
+
     if (desktopMode) {
       window.innerWidth = 1440; // Set your desired desktop width
     } else {
       window.innerWidth = 1024; // Set your desired mobile width
     }
 
-    // Trigger a resize event to update the UI
     window.dispatchEvent(new Event('resize'));
   };
 
   return (
     <div>
-      <button onClick={toggleDesktopMode}>
-        Toggle Desktop Mode
+      <button onClick={toggleDesktopMode} className='bg-orange-500 p-4 text-lg font-bold'>
+        Click Here
       </button>
     </div>
   );
