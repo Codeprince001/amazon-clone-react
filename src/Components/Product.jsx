4fd2c46f8e208b05c3f539c57ceee922ae79908b
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-09 16:09:10 +0100

    feature: added new feature to Product Component

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -6,11 +6,11 @@ const Product = ({ title, description, imgUrl, price }) => {
       <div className="h-[250px] flex justify-center">
         <img className="h-full" src={imgUrl} />
       </div>
-      <div className="h-[30%]">
-        <div className="h-[45px] overflow-hidden text-black text-sm mt-1">{description}</div>
+      <div>
+        <div className="h-[44px] overflow-hidden text-black text-sm mt-1">{description}</div>
         <div className="flex justify-between items-center"><span>⭐⭐⭐⭐</span><span className="text-black text-xl font-medium">${price}</span></div>
-        <div className="text-center">
-          <button className="bg-amazon-yellow hover:bg-orange-500 text-sm p-2 mt-2 rounded-xl">Add to cart</button>
+        <div className="text-center mt-1">
+          <button className="bg-amazon-yellow hover:bg-orange-500 text-sm pr-8 pl-8 pt-1 pb-1 bottom-4 text-black rounded-xl font-medium">Add to cart</button>
         </div>
       </div>
     </div>
