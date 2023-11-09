31b23df9a8c526ed4a1459a6e411f850134e2ff7
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-09 14:46:42 +0100

    added context api for loction

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,28 @@ import './App.css';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import { Checkout } from './Components';
 import Home from './Components/Home/Home';
+import { FetchIPAdress, locationContext } from './Components/Location/IPAdress';
+import ProductDetail from './Components/ProductDetail';
+
+
 
 function App() {
 
+  const value = FetchIPAdress();
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/checkout'
-            element={<Checkout />} />
-        </Routes>
-      </BrowserRouter>
+      <locationContext.Provider value={value}>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/checkout'
+              element={<Checkout />} />
+
+            <Route path='/product'
+              element={<ProductDetail />} />
+          </Routes>
+        </BrowserRouter>
+      </locationContext.Provider>
     </div>
   );
 }
