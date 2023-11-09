a123c7e25cd958cb8982431075b4b9135ee46feb
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-09 14:49:42 +0100

    update card component

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({ title, link, img, imgNumber }) => {
           <div className="h-80 m-4"><img src={img} className="h-full w-full" /></div>
           <div className="text-xs xl:text-sm text-blue-500 ml-4 mb-3 mt-0"><Link to="#">{link}</Link></div>
         </div> :
-        <div className="image-card-grid text-black bg-white p-2 h-full">
+        <div className="image-card-grid text-black bg-white p-2">
           <div className="text-lg xl:text-xl font-semibold mt-4 ml-4 object-cover">{title}</div>
           <div className="grid grid-cols-2 m-4 h-80 gap-4 grid-4-img">
             <div><img className="h-[120px] w-full object-fill" src={img.img1} /><span className="text-xs grid-text ml-2">{img.headset}</span></div>
