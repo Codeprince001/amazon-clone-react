ed8d99d6056c08b8331809de33239e89c37badc8
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-18 09:33:02 +0100

    feat: added context and modified cheackout element

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import amazonHomeLogo from "../../assets/amazon.png";
 import "./header.css";
 import { Link } from "react-router-dom";
 import Search from '../Search';
 import { IPAdress, FlagIcon } from '../Location/IPAdress';
 import { FiShoppingCart } from "react-icons/fi";
+import { useStateContext } from '../StateProvider';
 
 
 const Header = () => {
+
+  const [{ basket }] = useStateContext();
+
   return (
     <>
       <nav className='header'>
@@ -35,9 +39,8 @@ const Header = () => {
           </Link>
 
           <Link to="/checkout" className='header-link'>
-            <div className='header-shopping-option'>
-              <span className='cart'>{<FiShoppingCart color='#fff' className='shopping-icon' />}<span>Cart</span></span>
-              <span className='shopping-item'>0</span>
+            <div className='header-shopping-option relative'>
+              <div className='cart'>{<FiShoppingCart color='#fff' className='shopping-icon' />} <span className='shopping-item absolute text-[14px] font-medium right-[50%] top-[-10px]'>{basket?.length}</span></div>
             </div>
           </Link>
         </div>
