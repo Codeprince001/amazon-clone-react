51bc621b42beef519fe7dddb90b931008ec3c1c0
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-18 09:42:07 +0100

    Setup global context and reducer for App

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { StateProvider } from './Components/StateProvider.jsx';
+import { initialState, reducer } from '../reducer.jsx';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />
+    </StateProvider>
   </React.StrictMode>,
 );
