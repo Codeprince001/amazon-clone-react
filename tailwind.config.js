cddb92eec9300f47453a15589fe174cb102c3cda
Codeprince001 <urangyorkwisdom@gmail.com>
2023-11-18 09:46:14 +0100

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,8 @@ export default {
         amazon: {
           background: "#ededed",
           yellow: "#FFCC4B",
+          amazonBtnYellow: "#FFD814",
+          amazonLink: "#007185",
           cart: "radial-gradient(circle, rgba(255,204,75,1) 0%, rgba(255,204,75,1) 47%, rgba(255,156,42,1) 100%);"
         }
       }
